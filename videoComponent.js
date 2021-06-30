dc058370e6269cd3fb33ea6007f1696c0ce94944
Pragya Agrawal <56079032+pragya-01@users.noreply.github.com>
2021-06-30 06:57:05 +0530

    Add files via upload

---
+++ videoComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import './videoComponent.css';
+import {FaPlayCircle} from 'react-icons/fa'
+import {FaPauseCircle} from 'react-icons/fa'
+import video from './video.mp4'
+import PosterImageVideoComponent from './posterImage.jpg'
+
+export default class videoComponent extends Component
+{
+    
+    constructor(props) 
+    {
+        super(props);
+        this.state={isTogglePaused: true};
+        this.playVideo = this.playVideo.bind(this);
+    }
+
+   playVideo() 
+    {
+        alert('Click Happened');
+        if(this.state.isTogglePaused){
+        this.setState(prevState => ({
+            isTogglePaused :!prevState.isTogglePaused
+        }));
+    }
+    }
+
+    render()
+    {
+        let className='ContentVideo';
+        if(!this.state.isTogglePaused)
+       { className='ContentVideoPlay';}
+
+        return(
+   <div className="videoComponentLandingPage">
+    <video className="videoLandingPage" id="video1" src={video} type="video/mp4" 
+    poster={PosterImageVideoComponent} />
+    <div className={className}>
+    <button className="playVideoButtonLandingPage" id="playVideoButtonLandingPage" 
+    onClick={this.playVideo}><FaPlayCircle/> </button>
+           <p>
+               Watch Video
+           </p>
+            <p>
+                Unlock Your Potential
+            </p>
+        <p>
+          Introduce your video by telling yur visitors how they'll benefit  
+          by watching this short clip. Provide a brief summary of the content.
+        </p>
+    </div>
+</div>
+
+        );
+    }
+
+    }; 
+    /* poster={PosterImageVideoComponent}
+     
+
+        if((this).paused)
+                {
+                    (this).play();
+                    (this).removeClass('blurEffect');
+                    ('.contentVideo').hide();
+                }
+    
+    */
